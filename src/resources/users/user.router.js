319d1fa830b1ba3bb2cc6663ee0d29f98d446c18
Alexander <renyreny125tt@gmail.com>
2021-12-03 12:26:20 +0300

    feat: add user router

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,91 @@
-const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const { getUserAll, getUserId, addUser, updateUser, deleteUser } = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+const user = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    name: { type: 'string' },
+    login: { type: 'string' }
+  }
+};
 
-module.exports = router;
+const getUsersOpts = { 
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: user
+      }
+    }
+  },
+  handler: getUserAll
+};
+
+const getUserOpts = {
+  schema: {
+    response: {
+      200: user 
+    }
+  },
+  handler: getUserId
+};
+
+const addUserOpts = {
+  schema: {
+    body: {
+      type: 'object',
+      // required: ['name', 'login', 'password'],
+      properties: {
+        name: { type: 'string' },
+        login: { type: 'string' },
+        password: { type: 'string' },
+      },
+    },
+    response: {
+      201: user 
+    }
+  },
+  handler: addUser
+};
+
+const deleteUserOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: deleteUser,
+}
+
+const updateUserOpts = {
+  schema: {
+    response: {
+      200: user,
+    },
+  },
+  handler: updateUser,
+}
+
+
+const userRoutes = (app, options, done) => {
+  app.get('/', async (request, reply) => reply.send({ Message: 'Service: is running!' }));
+
+  app.get('/users', getUsersOpts);
+
+  app.get('/users/:userId', getUserOpts);
+
+  app.post('/users', addUserOpts);
+  
+  app.put('/users/:userId', updateUserOpts);  
+
+  app.delete('/users/:userId', deleteUserOpts);
+
+  done();
+};
+
+module.exports = userRoutes;
