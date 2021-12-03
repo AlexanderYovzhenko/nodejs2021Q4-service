088ad4b3b201554b7bbe35a9abdeea531045f784
Alexander <renyreny125tt@gmail.com>
2021-12-03 12:30:32 +0300

    feat: add user repository

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,19 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
-};
+let dbUsers = [];
 
-module.exports = { getAll };
+const getUsersAll = async () => dbUsers;
+
+const getUserId = async (id) => dbUsers.find(el => el.id === id);
+
+const addUser = async (user) => dbUsers.push(user);
+
+const updateUser = async (userId, updUser) => {dbUsers = dbUsers.map(el => el.id === userId ? updUser : el)};
+
+const deleteUser = async (userId) => {dbUsers = dbUsers.filter(el => el.id !== userId)};
+
+module.exports = { 
+  getUsersAll,
+  getUserId,
+  addUser,
+  updateUser,
+  deleteUser
+};
