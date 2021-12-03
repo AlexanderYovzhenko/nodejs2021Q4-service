6c20fe9292bc2ae466902d470883d0e33f879498
Alexander <renyreny125tt@gmail.com>
2021-12-03 12:27:00 +0300

    feat: add user servise

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,42 @@
+const { v4: uuid } = require('uuid')
 const usersRepo = require('./user.memory.repository');
+const { updateUserId } = require('../tasks/task.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getUserAll = async (request, reply) => {
+  const users = await usersRepo.getAll();
+  reply.code(200).send(users);
+};
 
-module.exports = { getAll };
+const getUserId = async (request, reply) => {
+  const { userId } = request.params;
+  const user = await usersRepo.getId(userId);
+  reply.code(200).send(user);
+};
+
+const addUser = async (request, reply) => {
+  const user = {
+    id: uuid(),
+    ...request.body
+  };
+  usersRepo.addUser(user);
+  reply.code(201).send(user);
+};
+
+const updateUser = async (request, reply) => {
+  const { userId } = request.params;
+  const updUser = {
+    id: userId,
+    ...request.body
+  };
+  usersRepo.updateUser(userId, updUser);
+  reply.code(200).send(updUser);
+};
+
+const deleteUser = async (request, reply) => {
+  const { userId } = request.params;
+  usersRepo.deleteUser(userId);
+  await updateUserId(userId);
+  reply.code(204);
+};
+
+module.exports = { getUserAll, getUserId, addUser, updateUser, deleteUser };
