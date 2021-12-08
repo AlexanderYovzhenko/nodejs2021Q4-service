2e37792aaec2025f9a2e2913b0df07d1be7f82bf
Alexander <renyreny125tt@gmail.com>
2021-12-08 20:24:33 +0300

    init: start typescript basics

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+const { PORT } = require('./common/config');
+const server = require('./app');
+
+// Run the server!
+const startServer = async () => {
+  try {
+    await server.listen(PORT, () =>
+      console.info(`App is running on http://localhost:${PORT}`)
+    );
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
