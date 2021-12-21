0425321e12144a5e8f4a1b4d71e5fc52fc5b7c75
Alexander <renyreny125tt@gmail.com>
2021-12-21 21:25:38 +0300

    feat: update file, switch to es6 modules

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,14 +1,17 @@
-import path from 'path';
-const dotenv = require('dotenv');
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-module.exports = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  AUTH_MODE: process.env.AUTH_MODE === 'true',
-};
+const PORT = process.env.PORT || 4000,
+  NODE_ENV = process.env.NODE_ENV,
+  MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY = process.env.JWT_SECRET_KEY,
+  AUTH_MODE = process.env.AUTH_MODE === 'true';
+
+export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE };
