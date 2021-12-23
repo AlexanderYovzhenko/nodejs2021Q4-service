27a6e75d9dc8118166857fecd164fb4d941b91f7
Alexander <renyreny125tt@gmail.com>
2021-12-23 21:17:03 +0300

    feat: add variable level log

--- src/common/config.ts
+++ src/common/config.ts
@@ -12,6 +12,14 @@ const PORT = process.env.PORT || 4000,
   NODE_ENV = process.env.NODE_ENV,
   MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY = process.env.JWT_SECRET_KEY,
-  AUTH_MODE = process.env.AUTH_MODE === 'true';
+  AUTH_MODE = process.env.AUTH_MODE === 'true',
+  LEVEL_LOG = process.env.LEVEL_LOG || '4';
 
-export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY, AUTH_MODE };
+export {
+  PORT,
+  NODE_ENV,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+  AUTH_MODE,
+  LEVEL_LOG,
+};
