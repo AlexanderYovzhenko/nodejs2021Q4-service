44366a01180bda255242bce6381780579878192a
Alexander <renyreny125tt@gmail.com>
2022-01-21 22:35:57 +0300

    refactor: add variable salt and jwt_key

--- src/common/config.ts
+++ src/common/config.ts
@@ -8,14 +8,15 @@ dotenv.config({
 const PORT = process.env.PORT || 4000,
   NODE_ENV = process.env.NODE_ENV,
   MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING,
-  JWT_SECRET_KEY = process.env.JWT_SECRET_KEY,
+  JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret-key',
   AUTH_MODE = process.env.AUTH_MODE === 'true',
   LEVEL_LOG = process.env.LEVEL_LOG || '4',
   PORT_DB = process.env.PORT_DB,
   POSTGRES_DB = process.env.POSTGRES_DB,
   DB_HOST = process.env.DB_HOST,
   POSTGRES_USER = process.env.POSTGRES_USER,
-  POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+  POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD,
+  SALT_HASH_PASSWORD = process.env.SALT_HASH_PASSWORD;
 
 export {
   PORT,
@@ -29,4 +30,5 @@ export {
   DB_HOST,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
+  SALT_HASH_PASSWORD,
 };
