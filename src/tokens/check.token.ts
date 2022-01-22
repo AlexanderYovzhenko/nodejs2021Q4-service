cd5d4ffb52deede1d8873d87fa5ac8c586d26103
Alexander <renyreny125tt@gmail.com>
2022-01-22 17:59:42 +0300

    refactor: upadate file

--- src/tokens/check.token.ts
+++ src/tokens/check.token.ts
@@ -2,7 +2,6 @@ import { FastifyRequest, FastifyReply, FastifyError } from 'fastify';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET_KEY } from '../common/config';
 import statusCode from '../common/status.code';
-import { logger, getLogObject } from '../logging/logger';
 
 const checkToken = (
   request: FastifyRequest,
@@ -10,35 +9,27 @@ const checkToken = (
   done: (err?: FastifyError) => void
 ) => {
   const path = request.routerPath;
+  const autHeader = request.headers.authorization;
 
   if (path === '/login' || path.includes('/doc', 0) || path === '/')
     return done();
 
-  const autHeader = request.headers.authorization;
-
   if (autHeader === undefined) {
     reply.status(statusCode.UNAUTHORIZED).send('Authorization Error');
-    logger.error(getLogObject(request, reply));
   } else {
     const [type, token] = autHeader.split(' ');
 
-    if (type !== 'Bearer') {
-      reply.status(statusCode.UNAUTHORIZED).send('Bearer Error');
-      logger.error(getLogObject(request, reply));
-    } else {
-      if (!token) {
-        reply.status(statusCode.UNAUTHORIZED).send('No token');
-        logger.error(getLogObject(request, reply));
-      } else {
-        try {
-          jwt.verify(token, JWT_SECRET_KEY);
-        } catch (error) {
-          reply.status(statusCode.UNAUTHORIZED).send('Invalid token');
-          logger.error(getLogObject(request, reply));
-        }
+    if (type === 'Bearer' && token) {
+      try {
+        jwt.verify(token, JWT_SECRET_KEY);
+      } catch (error) {
+        reply.status(statusCode.UNAUTHORIZED).send('Authorization Error');
       }
+    } else {
+      reply.status(statusCode.UNAUTHORIZED).send('Authorization Error');
     }
   }
+
   done();
 };
 
