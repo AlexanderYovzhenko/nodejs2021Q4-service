fee7181087d39c4bdcfb8f4e91cbea779f898ee3
Alexander <renyreny125tt@gmail.com>
2022-01-30 22:19:36 +0300

    feat: add logger

--- src/utils/logger.middleware.ts
+++ src/utils/logger.middleware.ts
@@ -6,21 +6,20 @@ export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { method, originalUrl, params, query, body } = request;
-    const { statusCode } = response;
-    const service = 'REST-service';
-
-    const messageLog = `${method} Url:${originalUrl} statusCode:${statusCode} params:${JSON.stringify(
-      params,
-    )} queryParams:${JSON.stringify(query)} body:${JSON.stringify(
-      body,
-    )} service:${service}`;
-
     response.on('finish', () => {
-      if (response.statusCode < 400) {
-        this.logger.log(messageLog);
-      } else {
+      const { method, originalUrl, params, query, body } = request;
+      const { statusCode } = response;
+      const service = 'REST-service';
+
+      const messageLog = `${method} Url:${originalUrl} statusCode:${statusCode} params:${JSON.stringify(
+        params,
+      )} queryParams:${JSON.stringify(query)} body:${JSON.stringify(
+        body,
+      )} service:${service}`;
+      if (response.statusCode >= 400) {
         this.logger.error(messageLog);
+      } else {
+        this.logger.log(messageLog);
       }
     });
 
